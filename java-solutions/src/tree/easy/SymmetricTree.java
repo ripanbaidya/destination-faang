9473854d9f10fae481d675b858228df51cdffddf
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-07-26 16:29:48 +0530

    add tree question

---
+++ java-solutions/src/tree/easy/SymmetricTree.java
@@ -0,0 +1,36 @@
+package tree.easy;
+
+import tree.representation.BinaryTreeMain;
+import tree.representation.TreeNode;
+
+/**
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ *
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ *
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ */
+public class SymmetricTree {
+    public boolean isSame(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+
+        return isSame(p.left, q.right) && isSame(p.right, q.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root.left == null && root.right == null) return true;
+
+        return isSame(root.left, root.right);
+    }
+
+    public static void main(String[] args) {
+        SymmetricTree obj = new SymmetricTree();
+
+        boolean isSymmetric = obj.isSymmetric(BinaryTreeMain.buildTree());
+        System.out.println("Is the tree symmetric? " + isSymmetric);
+    }
+}
---
+++ java-solutions/src/tree/medium/TopViewOfBinaryTree.java
@@ -0,0 +1,67 @@
+package tree.medium;
+
+import tree.representation.BinaryTreeMain;
+import tree.representation.Node;
+
+import java.util.*;
+
+/**
+ * You are given a binary tree, and your task is to return its top view. The top view of a binary tree
+ * is the set of nodes visible when the tree is viewed from the top.
+ *
+ * Note:
+ * Return the nodes from the leftmost node to the rightmost node.
+ * If two nodes are at the same position (horizontal distance) and are outside the shadow of the tree,
+ * consider the leftmost node only.
+ *
+ * Input: root[] = [1, 2, 3]
+ * Output: [2, 1, 3]
+ */
+public class TopViewOfBinaryTree {
+    class Tuple {
+        Node node;
+        int row;
+        int col;
+
+        public Tuple(Node node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(root == null) return result;
+
+        TreeMap<Integer, Integer> mp = new TreeMap<>(); // {col, topValue}
+        Queue<Tuple> q = new LinkedList<>(); // Tuple: {node, row, col}
+        q.offer(new Tuple(root, 0, 0));
+
+        while(!q.isEmpty()) {
+            Tuple curr = q.poll();
+
+            Node currNode = curr.node;
+            int currRow = curr.row;
+            int currCol = curr.col;
+
+            // top view
+            mp.putIfAbsent(currCol, currNode.data);
+
+            // Push left and right children into the queue, when they exist
+            if(currNode.left != null) q.offer(new Tuple(currNode.left, currRow+1, currCol-1));
+            if(currNode.right != null) q.offer(new Tuple(currNode.right, currRow+1, currCol+1));
+        }
+
+        // Extracting values from the TreeMap to maintain order
+        result.addAll(mp.values());
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TopViewOfBinaryTree obj = new TopViewOfBinaryTree();
+
+        List<Integer> topView = obj.topView(BinaryTreeMain.buildBinaryTree());
+        System.out.println("Top view of the binary tree: " + topView);
+    }
+}
