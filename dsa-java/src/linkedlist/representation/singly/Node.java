bb51069ffdcb965abe5134ccdb2706db6828f3c0
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-07-31 16:04:20 +0530

    add linked list representation

---
+++ dsa-java/src/linkedlist/representation/singly/Node.java
@@ -0,0 +1,17 @@
+package linkedlist.representation.singly;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(){}
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
