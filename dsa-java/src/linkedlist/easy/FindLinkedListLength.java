d2083692754172da929ef7f2dde17c24b6a200bd
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-07-31 16:04:49 +0530

    add easy problems

---
+++ dsa-java/src/linkedlist/easy/FindLinkedListLength.java
@@ -0,0 +1,30 @@
+package linkedlist.easy;
+
+import linkedlist.representation.singly.Node;
+
+/**
+ * @author Ripan Baidya
+ * @date 31-07-2025
+ *
+ * Given head of a singly linked list. The task is to find the length of the linked list,
+ * where length is defined as the number of nodes in the linked list.
+ *
+ * Input: head : 1->2->3->4->5
+ * Output: 5
+ * Explanation: Length of the linked list is 5, as there
+ * are 5 nodes present in it.
+ */
+public class FindLinkedListLength {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        Node temp = head; // temporary pointer
+        int len = 0; // count linked list length
+
+        while(temp != null){
+            len ++;
+            temp = temp.next;
+        }
+
+        return len;
+    }
+}
