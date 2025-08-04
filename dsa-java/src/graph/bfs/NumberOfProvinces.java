225fafb77da9b535fee40e629db75e825b765eb2
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-04 09:49:42 +0530

    add number of provinces using bfs

--- dsa-java/src/graph/bfs/NumberOfProvinces.java
+++ dsa-java/src/graph/bfs/NumberOfProvinces.java
@@ -4,36 +4,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Here, we will solve the problem of numberOfComponentsing the number of provinces in a given graph using Breadth First Search (BFS).
+ * @author Ripan Baidya
+ * @date 04-08-2025
+ *
+ * There are n cities. Some of them are connected, while some are not. If city a is connected
+ * directly with city b, and city b is connected directly with city c, then city a is connected
+ * indirectly with city c.
+ * A province is a group of directly or indirectly connected cities and no other cities outside
+ * of the group.
+ * You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the
+ * jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+ *
+ * Return the total number of provinces.
+ *
+ * Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+ * Output: 2
+ *
+ * time: O(V + 2E)
+ * space: O(V), visited array and queue
  */
 public class NumberOfProvinces {
     private void bfs(int node, int[][] isConnected, boolean[] vis) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.offer(node);
+        // mark the source node as visited
         vis[node] = true;
 
-        while(!q.isEmpty()) {
-            node = q.poll();
+        // declare a queue and put source node to it
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
 
-            for(int j = 0; j < isConnected.length; j ++) {
-                if(isConnected[node][j] == 1 && !vis[j]) {
-                    q.offer(j);
-                    vis[j] = true;
+        while(!q.isEmpty()) {
+            int u = q.poll();
+
+            for(int v = 0; v < isConnected.length; v ++) {
+                if(isConnected[u][v] == 1 && !vis[v]) {
+                    vis[v] = true;
+                    q.offer(v);
                 }
             }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
-        int V = isConnected.length;
-        boolean[] vis = new boolean[V];
-        int numberOfComponents = 0;
+        int V = isConnected.length; // number of vertices
+        boolean[] vis = new boolean[V]; // visited array
+        int numberOfComponents = 0; // number of component
 
         for(int i = 0; i < V; i ++)
             if(!vis[i]) {
                 bfs(i, isConnected, vis);
-                numberOfComponents ++;
+                numberOfComponents ++; // count component
             }
 
         return numberOfComponents;
