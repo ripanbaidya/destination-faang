508a80fb4d5bb5a55a7eb058cfb642edbfd2f2c3
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-04 09:50:26 +0530

    add number of provinces using dfs

--- dsa-java/src/graph/dfs/NumberOfProvinces.java
+++ dsa-java/src/graph/dfs/NumberOfProvinces.java
@@ -1,7 +1,24 @@
 package graph.dfs;
 
 /**
- * Here, we will solve the problem of counting the number of provinces in a given graph using Depth First Search (DFS).
+ * @author Ripan Baidya
+ * @date 04-08-2025
+ *
+ * There are n cities. Some of them are connected, while some are not. If city a is connected
+ * directly with city b, and city b is connected directly with city c, then city a is connected
+ * indirectly with city c.
+ * A province is a group of directly or indirectly connected cities and no other cities outside
+ * of the group.
+ * You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the
+ * jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+ *
+ * Return the total number of provinces.
+ *
+ * Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+ * Output: 2
+ *
+ * time: O(V + 2E)
+ * space: O(V), visited array and queue
  */
 public class NumberOfProvinces {
     public void dfs(int node, int[][] isConnected, boolean[] visit) {
