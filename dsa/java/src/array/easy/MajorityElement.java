08ee6e646f9d4aa7ac60a77e3f4dfcbc005404c8
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-06 21:28:55 +0530

    majority element

--- dsa/java/src/array/easy/MajorityElement.java
+++ dsa/java/src/array/easy/MajorityElement.java
@@ -4,9 +4,9 @@ package array.easy;
  * @author Ripan Baidya
  * @date 31-07-2025
  *
- * Given an array nums of size n, return the majority element.
- * The majority element is the element that appears more than ⌊n / 2⌋ times.
- * You may assume that the majority element always exists in the array.
+ * Given an array nums of size n, return the majority element. cThe majority element is the
+ * element that appears more than ⌊n / 2⌋ times. You may assume that the majority element
+ * always exists in the array.
  *
  * Example :
  * Input: nums = [2,2,1,1,1,2,2]
