298fd62933ded5ebb94acfba877c1dfdc4cbffd2
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-06 11:20:43 +0530

    Dijkstra Algorithm using priority queue

--- dsa/java/src/graph/hard/DijkstraAlgorithmUsingPriorityQueue.java
+++ dsa/java/src/graph/hard/DijkstraAlgorithmUsingPriorityQueue.java
@@ -9,17 +9,18 @@ import java.util.stream.IntStream;
  * @author Ripan Baidya
  * @date 04-08-2025
  *
- * Given an undirected, weighted graph with V vertices numbered from 0 to V-1 and E edges, represented by 2d array edges[][],
- * where edges[i]=[u, v, w] represents the edge between the nodes u and v having w edge weight.
- * Find the shortest distance of all the vertices from the source vertex src, and return an array of integers where the ith
- * element denotes the shortest distance between ith node and source vertex src.
+ * Given an undirected, weighted graph with V vertices numbered from 0 to V-1 and E edges,
+ * represented by 2d array edges[][], where edges[i]=[u, v, w] represents the edge between
+ * the nodes u and v having w edge weight.
+ * Find the shortest distance of all the vertices from the source vertex src, and return an
+ * array of integers where the ith element denotes the shortest distance between ith node
+ * and source vertex src.
  * Note: The Graph is connected and doesn't contain any negative weight edge.
  *
  * Input: V = 5, edges[][] = [[0, 1, 4], [0, 2, 8], [1, 4, 6], [2, 3, 2], [3, 4, 10]], src = 0
  * Output: [0, 4, 8, 10, 10]
- * Explanation:
  *
- * Shortest Paths:
+ * Explanation:
  * For 0 to 1 minimum distance will be 4. By following path 0 -> 1
  * For 0 to 2 minimum distance will be 8. By following path 0 -> 2
  * For 0 to 3 minimum distance will be 10. By following path 0 -> 2 -> 3
@@ -28,14 +29,17 @@ import java.util.stream.IntStream;
  * time complexity: O(E * logV)
  * space complexity: O(V)
  */
-
 public class DijkstraAlgorithmUsingPriorityQueue {
-    // convert 2D matrix to adjacency list
+    // convert edge list into an adjacency list
     private List<List<int[]>> convertToAdjList(int V, int[][] edges) {
         List<List<int[]>> adj = new ArrayList<>();
 
-        for (int i = 0; i < V; i ++) adj.add(new ArrayList<>());
+        // initialize adjacency list
+        for (int i = 0; i < V; i ++) {
+            adj.add(new ArrayList<>());
+        }
 
+        // add directed edges with weights to the list
         for (int[] edge : edges) {
             int u = edge[0], v = edge[1], weight = edge[2];
             adj.get(u).add(new int[]{v, weight});
@@ -43,42 +47,45 @@ public class DijkstraAlgorithmUsingPriorityQueue {
 
         return adj;
     }
-    // dijkstra algorithm using priority queue(min-heap)
+
+    // Dijkstra Algorithm using priority queue(min-heap)
     public int[] dijkstra(int V, int[][] edges, int src) {
         List<List<int[]>> adj = convertToAdjList(V, edges);
 
-        // {distance, node}
+        // each element will be {distance, node}
         // sort based on distance, if equal then based on node.
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[0] != b[0]) return Integer.compare(a[0], b[0]); // Compare distance
-            return Integer.compare(a[1], b[1]); // Tie-break: compare node
+            return (a[0] != b[0]) ? Integer.compare(a[0], b[0])
+                                  : Integer.compare(a[1], b[1]);
         });
 
-        int[] dist = new int[V]; // distance array
-        Arrays.fill(dist, (int) 1e9); // initialize all distances to infinity(1e9)
-        dist[src] = 0; // source to source is distance 0
-        pq.offer(new int[]{0, src}); // {distance, node}
+        // declare and initialize distance array
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int) 1e9);
 
-        // bfs
+        // put the source into priority queue and set its distance to 0
+        dist[src] = 0;
+        pq.offer(new int[]{0, src});
+
+        // BFS
         while (!pq.isEmpty()) {
-            int edgeWeight = pq.peek()[0];
-            int node = pq.peek()[1];
-
-            pq.poll();
+            int[] top = pq.poll();
+            int currDist = top[0];
+            int currNode = top[1];
 
             // traverse through all adjacent nodes
-            for (int[] neighbor : adj.get(node)) {
-                int v = neighbor[0];
-                int wt = neighbor[1];
+            for (int[] neighbor : adj.get(currNode)) {
+                int adjNode = neighbor[0];
+                int edgeWeight = neighbor[1];
 
-                if (edgeWeight + wt < dist[v]) {
-                    dist[v] = edgeWeight + wt; // update distance with shorter distance
-                    pq.offer(new int[]{(edgeWeight + wt), v}); // put the new distance and node in the priority queue
+                if ((dist[currNode] + edgeWeight) < dist[adjNode]) {
+                    dist[adjNode] = dist[currNode] + edgeWeight; // update distance
+                    pq.offer(new int[]{dist[adjNode], adjNode}); // put into priority queue
                 }
             }
         }
 
-        // vertex that are unreachable from source will have distance -1
+        // mark all unreachable nodes as -1
         IntStream.range(0, V).forEach(i -> {
             if (dist[i] == (int) 1e9) {
                 dist[i] = -1;
@@ -87,6 +94,7 @@ public class DijkstraAlgorithmUsingPriorityQueue {
 
         return dist;
     }
+
     public static void main(String[] args) {
         var obj = new DijkstraAlgorithmUsingPriorityQueue();
 
@@ -98,8 +106,9 @@ public class DijkstraAlgorithmUsingPriorityQueue {
                 {3, 4, 10}
         };
         int V = edges.length;
+        int source = 0;
 
-        int[] shortestPath = obj.dijkstra(V, edges, 0); // V, edges, source
+        int[] shortestPath = obj.dijkstra(V, edges, source);
         System.out.println(Arrays.toString(shortestPath));
     }
 }
