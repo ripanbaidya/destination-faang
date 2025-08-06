20f7cb82dad71765b07b53a75f9b0a28fbda5383
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 00:58:17 +0530

    four sum

--- dsa/java/src/array/medium/FourSum.java
+++ dsa/java/src/array/medium/FourSum.java
@@ -8,29 +8,31 @@ import java.util.List;
  * @author Ripan Baidya
  * @date 31-07-2025
  *
- * Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
- * => 0 <= a, b, c, d < n
- * => a, b, c, and d are distinct.
- * => nums[a] + nums[b] + nums[c] + nums[d] == target
+ * Given an array nums of n integers, return an array of all the unique quadruplets
+ * [nums[a], nums[b], nums[c], nums[d]] such that:
+ * - 0 <= a, b, c, d < n
+ * - a, b, c, and d are distinct.
+ * - nums[a] + nums[b] + nums[c] + nums[d] == target
  * You may return the result  wer in any order.
  *
  * Example:
  * Input: nums = [1,0,-1,0,-2,2], target = 0
  * Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
  */
-
-
 public class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
 
+        // sort the array
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
 
+        List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < n; i ++){
+            // skip element if it is same as previous
             if (i > 0 && nums[i] == nums[i-1]) continue;
 
             for (int j = i+1; j < n; j ++){
+                // skip element if it is same as previous
                 if (j > i+1 && nums[j] == nums[j-1]) continue;
 
                 int k = j+1;
