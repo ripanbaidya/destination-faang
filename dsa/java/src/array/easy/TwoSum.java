327c03360c00080028089a7a08f44800b7bc399d
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-06 21:31:31 +0530

    two sum

--- dsa/java/src/array/easy/TwoSum.java
+++ dsa/java/src/array/easy/TwoSum.java
@@ -8,9 +8,9 @@ import java.util.Map;
  * @author Ripan Baidya
  * @date 31-07-2025
  *
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * You can return the answer in any order.
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that
+ * they add up to target. You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice. You can return the answer in any order.
  *
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
