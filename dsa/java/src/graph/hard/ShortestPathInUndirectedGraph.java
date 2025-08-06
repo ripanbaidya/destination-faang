2c29d033b3ce7f74e0a676885ce466dd16be0c0b
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-06 10:52:13 +0530

    shortest path in undirected graph using queue

--- dsa/java/src/graph/hard/ShortestPathInUndirectedGraph.java
+++ dsa/java/src/graph/hard/ShortestPathInUndirectedGraph.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.IntStream;
 
 /**
  * @author Ripan Baidya
  * @date 03-08-2025
  *
- * You are given an adjacency list, adj of Undirected Graph having unit weight of the edges, find the shortest
- * path from src to all the vertex and if it is unreachable to reach any vertex, then return -1 for that vertex.
+ * You are given an adjacency list, adj of Undirected Graph having unit weight of the edges,
+ * find the shortest path from src to all the vertex and if it is unreachable to reach any
+ * vertex, then return -1 for that vertex.
  *
- * Input: adj[][] = [[1, 3], [0, 2], [1, 6], [0, 4], [3, 5], [4, 6], [2, 5, 7, 8], [6, 8], [7, 6]], src=0
+ * Input:
+ * adj[][] = [[1, 3], [0, 2], [1, 6], [0, 4], [3, 5], [4, 6], [2, 5, 7, 8], [6, 8], [7, 6]]
+ * src=0
  * Output: [0, 1, 2, 1, 2, 3, 3, 4, 4]
  *
  * time: O(V + E)
@@ -23,27 +27,30 @@ public class ShortestPathInUndirectedGraph {
     public int[] shortestPath(ArrayList<ArrayList<Integer>> adjList, int source) {
         int V = adjList.size();
 
-        // distance array to store the shortest distance from the source to each currNode
+        // dist[i] will hold the shortest distance from source to i
         int[] dist = new int[V];
-        Arrays.fill(dist, (int) 1e9);  // Initially, set all distances to infinity
+
+        // Initialize all distances as infinite
+        Arrays.fill(dist, (int) 1e9);
 
         // Queue for BFS traversal: each element is {node, distance from source}
         Queue<int[]> queue = new LinkedList<>();
-        dist[source] = 0;  // source to source is distance 0
+
+        // source to source is distance 0
+        dist[source] = 0;
         queue.offer(new int[]{source, 0});
 
-        // bfs
+        // BFS
         while (!queue.isEmpty()) {
-            int node = queue.peek()[0];
-            int currDist = queue.peek()[1];
-            queue.poll();
+            int[] top = queue.poll();
+            int currNode = top[0];
+            int currDist = top[1];
 
-            int newDistance = currDist + 1;  // For unweighted graph, each edge adds +1
+            // Traverse through all adjacent nodes
+            for (int adjNode : adjList.get(currNode)) {
+                // Edge weight is 1
+                int newDistance = currDist + 1;
 
-            // Traverse all adjacent nodes
-            for (int adjNode : adjList.get(node)) {
-
-                // If a shorter path to adjNode is found
                 if (newDistance < dist[adjNode]) {
                     dist[adjNode] = newDistance;
                     queue.offer(new int[]{adjNode, newDistance});
@@ -51,12 +58,12 @@ public class ShortestPathInUndirectedGraph {
             }
         }
 
-        // Replace unreachable currNode distances with -1
-        for (int i = 0; i < V; i++) {
-            if (dist[i] == Integer.MAX_VALUE) {
-                dist[i] = -1;
-            }
-        }
+        // Convert all infinite distances to -1
+        IntStream.range(0, V).forEach(i -> {
+           if (dist[i] == (int) 1e9) {
+               dist[i] = -1;
+           }
+        });
 
         return dist;
     }
