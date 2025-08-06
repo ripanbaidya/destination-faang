55c81b6760ebed6f16fee2b0b996c4e608960b7a
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 00:57:15 +0530

    count intervals

--- dsa/java/src/array/hard/CountInversions.java
+++ dsa/java/src/array/hard/CountInversions.java
@@ -5,8 +5,10 @@ package array.hard;
  * @date 31-07-2025
  *
  * Given an array of integers arr[]. You have to find the Inversion Count of the array.
- * Note : Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+ * Note : Inversion count is the number of pairs of elements (i, j) such that i < j and
+ * arr[i] > arr[j].
  *
+ * Example:
  * Input: arr[] = [2, 4, 1, 3, 5]
  * Output: 3
  * Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
@@ -73,6 +75,7 @@ public class CountInversions {
 
         int[] arr = {2, 4, 1, 3, 5};
         int count = obj.inversionCount(arr);
+
         System.out.println("Inversion count: "+ count);
     }
 }
