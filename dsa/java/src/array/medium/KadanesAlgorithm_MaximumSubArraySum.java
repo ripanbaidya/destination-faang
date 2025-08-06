0600ff3b953b13e1568f59f2d68f373e9ba6ba55
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 00:58:50 +0530

    maximum sub-array sum- Kadane's Algorithm

--- dsa/java/src/array/medium/KadanesAlgorithm_MaximumSubArraySum.java
+++ dsa/java/src/array/medium/KadanesAlgorithm_MaximumSubArraySum.java
@@ -16,26 +16,29 @@ public class KadanesAlgorithm_MaximumSubArraySum {
     // optimal solution
     public int maxSubArray(int[] nums) {
         int n = nums.length;
-        int sum = 0;
+        int currentSum = 0;
         int maxiSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i ++){
-            sum += nums[i];
+            currentSum += nums[i];
 
             // update the maximum sum
-            maxiSum = Math.max(maxiSum, sum);
+            maxiSum = Math.max(maxiSum, currentSum);
 
             // reset the sum when it becomes negative
-            if(sum < 0) sum = 0;
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
         }
+
         return maxiSum;
     }
     public static void main(String[] args) {
-        KadanesAlgorithm_MaximumSubArraySum obj = new KadanesAlgorithm_MaximumSubArraySum();
+        var obj = new KadanesAlgorithm_MaximumSubArraySum();
 
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-
         int maximumSum = obj.maxSubArray(nums);
+
         System.out.println(maximumSum);
     }
 }
