59c13473f32af9e7149e1a1973d2073268d19f61
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 01:02:33 +0530

    next permutation

--- dsa/java/src/array/medium/NextPermutation.java
+++ dsa/java/src/array/medium/NextPermutation.java
@@ -7,18 +7,13 @@ import java.util.Arrays;
  * @date 31-07-2025
  *
  * A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
- * For example, for arr = [1,2,3], the following are all the permutations of
- * arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
- * The next permutation of an array of integers is the next lexicographically greater permutation of its integer.
- * More formally, if all the permutations of the array are sorted in one container according to their lexicographical order,
- * then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement
- * is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+ * For example, for arr = [1,2,3], permutations: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
  *
- * For example, the next permutation of arr = [1,2,3] is [1,3,2].
- * Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
- * While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
- * Given an array of integers nums, find the next permutation of nums.
- * The replacement must be in place and use only constant extra memory.
+ * The next permutation of an array of integers is the next lexicographically greater permutation of its
+ * integer. More formally, if all the permutations of the array are sorted in one container according to
+ * their lexicographical order, then the next permutation of that array is the permutation that follows it
+ * in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest
+ * possible order (i.e., sorted in ascending order).
  *
  * Example:
  * Input: nums = [1,2,3]
@@ -70,11 +65,11 @@ public class NextPermutation {
     }
 
     public static void main(String[] args) {
-        NextPermutation obj = new NextPermutation();
+        var obj = new NextPermutation();
 
         int[] nums = {1, 3, 2};
-        obj.nextPermutation(nums);
 
+        obj.nextPermutation(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
