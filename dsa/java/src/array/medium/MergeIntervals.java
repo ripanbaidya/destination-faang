577605a4b55e776fd59d0704485b926943940545
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 01:01:43 +0530

    merge intervals

--- dsa/java/src/array/medium/MergeIntervals.java
+++ dsa/java/src/array/medium/MergeIntervals.java
@@ -11,6 +11,7 @@ import java.util.List;
  * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
  * and return an array of the non-overlapping intervals that cover all the intervals in the input.
  *
+ * Example:
  * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
  * Output: [[1,6],[8,10],[15,18]]
  * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
@@ -55,7 +56,7 @@ public class MergeIntervals {
     }
 
     public static void main(String[] args) {
-        MergeIntervals obj = new MergeIntervals();
+        var obj = new MergeIntervals();
         int[][] intervals = {{1, 3}, {2, 6},{8, 10},{15,18}};
 
         int[][] result = obj.merge(intervals);
