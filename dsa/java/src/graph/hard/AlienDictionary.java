43a0cf0614bc09f539bda61b149a063c29bb120e
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 19:38:53 +0530

    alien dictionary

--- dsa/java/src/graph/hard/AlienDictionary.java
+++ dsa/java/src/graph/hard/AlienDictionary.java
@@ -9,18 +9,22 @@ import java.util.Queue;
  * @author Ripan Baidya
  * @date 03-08-2025
  *
- * A new alien language uses the English alphabet, but the order of letters is unknown. You are given a list of words[]
- * from the alien language’s dictionary, where the words are claimed to be sorted lexicographically according to the
- * language’s rules.
- * Your task is to determine the correct order of letters in this alien language based on the given words. If the order
- * is valid, return a string containing the unique letters in lexicographically increasing order as per the new language's
- * rules. If there are multiple valid orders, return any one of them.
- * However, if the given arrangement of words is inconsistent with any possible letter ordering, return an empty string ("").
- * A string a is lexicographically smaller than a string b if, at the first position where they differ, the character in a
- * appears earlier in the alien language than the corresponding character in b. If all characters in the shorter word match
- * the beginning of the longer word, the shorter word is considered smaller.
- * Note: Your implementation will be tested using a driver code. It will print true if your returned order correctly follows
- * the alien language’s lexicographic rules; otherwise, it will print false.
+ * A new alien language uses the English alphabet, but the order of letters is unknown. You are
+ * given a list of words[] from the alien language’s dictionary, where the words are claimed to
+ * be sorted lexicographically according to the language’s rules.
+ * Your task is to determine the correct order of letters in this alien language based on the given
+ * words. If the order is valid, return a string containing the unique letters in lexicographically
+ * increasing order as per the new language's rules. If there are multiple valid orders, return any
+ * one of them. However, if the given arrangement of words is inconsistent with any possible letter
+ * ordering, return an empty string ("").
+ * A string a is lexicographically smaller than a string b if, at the first position where they differ,
+ * the character in a appears earlier in the alien language than the corresponding character in b. If all
+ * characters in the shorter word match the beginning of the longer word, the shorter word is considered
+ * smaller.
+ * Note: Your implementation will be tested using a driver code. It will print true if your returned order
+ * correctly follows the alien language’s lexicographic rules; otherwise, it will print false.
+ *
+ * Example :
  *
  * Input: words[] = ["baa", "abcd", "abca", "cab", "cad"]
  * Output: true
@@ -102,5 +106,12 @@ public class AlienDictionary {
         }
 
         return sb.length() == totalSeen ? sb.toString() : "";
+    }
+
+    public static void main(String[] args) {
+        var obj = new AlienDictionary();
+
+        String[] words = {"baa", "abcd", "abca", "cab", "cad"};
+        System.out.println(obj.findOrder(words));
     }
 }
