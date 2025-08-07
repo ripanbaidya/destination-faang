b068fbe03331f40df1fc38cd6aa321d58dec2eb4
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-07 19:56:10 +0530

    bipartite graph

--- dsa/java/src/graph/medium/BipartiteGraph.java
+++ dsa/java/src/graph/medium/BipartiteGraph.java
@@ -8,22 +8,27 @@ import java.util.List;
  * @author Ripan Baidya
  * @date 31-07-2025
  *
- * Given a Graph with V vertices (Numbered from 0 to V-1) and E edges. Check whether the graph is bipartite or not.
- * A bipartite graph can be colored with two colors such that no two adjacent vertices share the same color. This
- * means we can divide the graph’s vertices into two distinct sets where:
+ * Given a Graph with V vertices (Numbered from 0 to V-1) and E edges. Check whether the
+ * graph is bipartite or not. A bipartite graph can be colored with two colors such that
+ * no two adjacent vertices share the same color. This means we can divide the graph’s
+ * vertices into two distinct sets where:
  *
- * All edges connect vertices from one set to vertices in the other set.
- * No edges exist between vertices within the same set.
+ * All edges connect vertices from one set to vertices in the other set. No edges exist
+ * between vertices within the same set.
+ *
+ * Example:
  *
  * Input: V = 3, edges[][] = [[0, 1], [1,2]], Bipartite-Graph
  * Output: true
  * Explanation: The given graph can be colored in two colors so, it is a bipartite graph.
  */
 public class BipartiteGraph {
-    // Convert the edge list to an adjacency list representation
+    // convert edge list into an adjacency list
     private List<List<Integer>> convertToAdjList(int V, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i ++) adj.add(new ArrayList<>());
+        for(int i = 0; i < V; i ++) {
+            adj.add(new ArrayList<>());
+        }
 
         for(int[] edge : edges) {
             int u = edge[0];
@@ -34,6 +39,7 @@ public class BipartiteGraph {
 
         return adj;
     }
+
     private boolean dfs(int src, int color, List<List<Integer>> adjList, int[] colors) {
         colors[src] = color;
 
@@ -41,8 +47,9 @@ public class BipartiteGraph {
             if (colors[adjNode] == -1) {
                 // If we found any node return false means it is not bipartite
                 // we also return false to its parent node
-                if (!dfs(adjNode, color^1, adjList, colors))
+                if (!dfs(adjNode, color^1, adjList, colors)){
                     return false;
+                }
             }
             // When the adjacent node is already colored with the same color
             else if (colors[adjNode] == color) {
@@ -55,6 +62,7 @@ public class BipartiteGraph {
     }
     public boolean isBipartite(int V, int[][] edges) {
         List<List<Integer>> adj = convertToAdjList(V, edges);
+
         int[] colors = new int[V];
         Arrays.fill(colors, -1);
 
@@ -66,5 +74,15 @@ public class BipartiteGraph {
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        var obj = new BipartiteGraph();
+
+        int V = 3;
+        int[][] edges = {{0, 1}, {1, 2}}; // bipartite graph
+
+        boolean isBipartiteGraph = obj.isBipartite(V, edges);
+        System.out.println(isBipartiteGraph);
     }
 }
