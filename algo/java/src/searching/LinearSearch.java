c2e0d82280f7411454ee9543c5a03b1446eaf9a6
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-09 14:59:34 +0530

    linear search

--- algo/java/src/searching/LinearSearch.java
+++ algo/java/src/searching/LinearSearch.java
@@ -4,7 +4,7 @@ package searching;
  * @author Ripan Baidya
  * @date 30-07-2025
  *
- * Given an array, arr[] of n integers, and an integer element x, find whether element
+ * Given an array, arr[] of n integers, and an integer element x , find whether  element
  * x is present in the array. Return the index of the first occurrence of x in the array,
  * or -1 if it doesn't exist.
  *
